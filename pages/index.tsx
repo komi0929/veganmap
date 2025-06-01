60e682585b20d34e64ad95eb15b9655e2285eca6
komi0929 <y.kominami@hitokoto1.co.jp>
2025-06-02 08:05:57 +0900

    Create index.tsx

---
+++ pages/index.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{height:"100vh",display:"flex",alignItems:"center",justifyContent:"center"}}>
+      <h1>Hello VeganMap!</h1>
+      <p>この画面が出れば Next.js が正しく動いています。</p>
+    </div>
+  );
+}
